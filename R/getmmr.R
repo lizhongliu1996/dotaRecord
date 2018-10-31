eaa6de086cab7cfcfa56892b7d9db7dfaceeb2e1
Lizhong Liu <lxl816@miami.edu>
2018-10-30 23:46:34 -0400

    add function to get your gameID and mmr score

---
+++ R/getmmr.R
@@ -0,0 +1,22 @@
+#' getmmr function returns a dataframe with your nickname and matchmaking rate score when passed dota2 id
+#'
+#' @param dota2ID your dota2 account ID
+#'
+#' @return NULL
+#' @import ggplot2 ROpenDota
+#'
+#' @export
+#'
+#' @examples
+#' getmmr(135474549)
+#'
+#'
+#'
+getmmr<-function(dota2ID){
+   e<-get_player_summaries(dota2ID)
+   score<-as.data.frame(e$mmr_estimate$estimate)
+   name<-as.data.frame(e$profile$personaname)
+   mmr<-cbind(name,score)
+   colnames(mmr)<-c("GameID","MMR score")
+   return(mmr)
+}
